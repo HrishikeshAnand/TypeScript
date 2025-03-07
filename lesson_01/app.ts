e710348651f4be48de890939df2583083b49b37b
Hrishikesh <hrishikeshanand3@gmail.com>
2025-03-07 10:27:38 +0530

    typescript program with type annotation

---
+++ lesson_01/app.ts
@@ -0,0 +1,23 @@
+function processFeedback(input: unknown) {
+    if(typeof input === 'string') {
+        console.log(`Processing text: ${input}`);
+    }
+    else if(typeof input === 'number') {
+        console.log(`Processing number: ${input}`);
+    }
+    else if(typeof input === 'boolean') {
+        console.log(`Processing states: ${input}`);
+    }
+    else {
+        console.log(`${input} is unsupported type of input`);
+    }
+}
+console.log();
+processFeedback('Great Service!');
+processFeedback(true);
+processFeedback(404);
+processFeedback(undefined);
+processFeedback(null);
+console.log();
+
+
